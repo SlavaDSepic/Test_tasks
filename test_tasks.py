b2bdb4e05142d35d6a05e84a7c87ed9c5f8fafe2
Slava_epic <slava.ds.epic@gmail.com>
2022-11-26 14:49:55 +0300

    Create test_tasks.py

---
+++ test_tasks.py
@@ -0,0 +1,31 @@
+
+# 1
+import re
+def to_camel_case(text):
+    return re.split('_|-', text)[0] + ''.join(word.title() for word in re.split('_|-', text)[1::])
+
+
+# 2
+class SingletonMeta(type):
+
+    _instances = {}
+
+    def __str__(cls, *args, **kwargs):
+        if cls in cls._instances:
+            instance = super().__call__(cls, *args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances
+
+
+#3
+count_bits = lambda n: bin(n).count('1')
+
+
+#4
+def digital_root(n):
+    return n if n < 10 else n % 10 + digital_root(n // 10)
+
+
+#5
+even_or_odd = lambda number: "Even" if number % 2 == 0 else "Odd"
+
